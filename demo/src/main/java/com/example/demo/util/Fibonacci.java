383db6dbaa6f4d71ea8b470f51eb53b7d0105149
Hugo Vantighem <hugo.vantighem2@altran.com>
2019-11-19 14:35:03 +0100

    Fibonacci

--- demo/src/main/java/com/example/demo/util/Fibonacci.java
+++ demo/src/main/java/com/example/demo/util/Fibonacci.java
@@ -4,12 +4,8 @@ package com.example.demo.util;
 public class Fibonacci {
 
     public int compute(int rank){
-        if(rank == 0){
-            return 0;
-        }
-        if(rank == 1){
-            return 1;
-        }
-        return compute(rank - 1) + compute(rank - 2);
+        System.out.println("Fib compute " + rank);
+        if (rank <= 1) {return rank;}
+        else {return compute(rank-1) + compute(rank-2);}
     }
 }
